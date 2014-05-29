63f051840ee32603361cd4625c187daea188e6db
timsgardner <timsgardner@gmail.com>
2014-05-29 19:10:44 -0400

    first commit

---
+++ clj-test-project/test/clj_test_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-test-project.core-test
+  (:require [clojure.test :refer :all]
+            [clj-test-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
