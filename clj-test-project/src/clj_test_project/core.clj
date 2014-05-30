9a34d852b48d461c403c1022efc6eefd84f0682c
timsgardner <timsgardner@gmail.com>
2014-05-29 20:17:38 -0400

    writing some tests, doin it

--- clj-test-project/src/clj_test_project/core.clj
+++ clj-test-project/src/clj_test_project/core.clj
@@ -1,6 +1,12 @@
-(ns clj-test-project.core)
+(ns clj-test-project.core
+  (:require [clojure.pprint :as pprint]
+            [clojure.repl :as repl]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn f-1 [x] x)
+
+(defn f-2 [x]
+  {::type :some-type
+   :value (f-1 x)})
+
+(defn f-3 [x]
+  (merge (f-2 x) {:should-be (f-1 x)}))
